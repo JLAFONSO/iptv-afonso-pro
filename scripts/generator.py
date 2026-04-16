cdb15b9155da0a2c73d26dcfd884b91fa703165d
JLAFONSO <63168255+JLAFONSO@users.noreply.github.com>
2026-04-16 23:54:00 +0100

    Add script to download and save IPTV playlist

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,1 +1,12 @@
+import requests
 
+url = "https://iptv-org.github.io/iptv/index.m3u"
+
+print("A descarregar playlist...")
+
+data = requests.get(url).text
+
+with open("playlists/full.m3u", "w", encoding="utf-8") as f:
+    f.write(data)
+
+print("Playlist gerada com sucesso!")
